f0018630b4527d680eb60d223cdccc6fefcae68f
Mildred Gil <mi.mitel@hotmail.com>
2020-05-25 04:59:29 -0500

    button right

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -6,7 +6,10 @@ import Header from '../Header/';
 import Footer from '../Footer/';
 import * as colors from '../../constants/colors';
 import { Helmet } from 'react-helmet';
-import { FormControl,InputLabel, Input, FormHelperText } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import { FormControl } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 const Login = ({classes}) => {
   document.title = "¿Quiénes Somos? | MexiCOVID";
@@ -19,13 +22,40 @@ const Login = ({classes}) => {
         <Header fixed={true}/>
         <div className={classes.teamsContainer}>
         <Typography className={classes.h1} variant={'h1'}>Bienvenido</Typography>
-        <FormControl>
-            <InputLabel htmlFor="correo">Correo Electronico</InputLabel>
-            <Input id="correo" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text">Ejemplo: investigador@asociacion.com</FormHelperText>
-            <InputLabel htmlFor="password">Contraseña</InputLabel>
-            <Input id="password" aria-describedby="my-helper-text" />
-        </FormControl>
+            <FormControl fullWidth>
+                <TextField
+                id="outlined-full-width"
+                label="Correo Electronico"
+                style={{ margin: 8 }}
+                placeholder="ejemplo@itesm.mx"
+                fullWidth
+                type="email"
+                margin="normal"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                variant="outlined"
+                />
+                <TextField
+                id="outlined-full-width"
+                label="Contraseña"
+                style={{ margin: 8 }}
+                placeholder="Placeholder"
+                helperText="8 caracteres"
+                fullWidth
+                margin="normal"
+                type="password"
+                required
+                minlength="8"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                variant="outlined"
+                />
+            </FormControl>
+            <Grid container item xs direction="row"  justify="space-around"  alignItems="right">
+                <Button className={classes.button}>Iniciar</Button>
+            </Grid>
         </div>
         <Footer/>
       </div>
@@ -41,6 +71,9 @@ const styles = () => ({
     backgroundColor: colors.GRAY,
 	},
   
+    button: {
+
+    },
   container: {
     width: '100%',
     backgroundColor: colors.GRAY,
