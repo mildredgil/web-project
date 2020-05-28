ab3c28525d558d41eaf7a599b59048ea48f1d8e1
mildredgil <mi.mitel@hotmail.com>
2020-05-28 01:03:10 -0500

    actualizacion regiones

--- client/src/App.js
+++ client/src/App.js
@@ -14,10 +14,6 @@ import { RegionContextProvider } from './contexts/RegionContext';
 import { HomeContextProvider } from './contexts/HomeContext';
 import { MapContextProvider } from './contexts/MapContext';
 import { MapMunicipioContextProvider } from './contexts/MapMunicipioContext';
-import AvailableBeds from './components/AvailableBeds';
-import ConfirmAge from './components/ConfirmAge';
-import MunicipalitiesFollow from './components/MunicipalitiesFollow';
-import DistributionEstate from './components/DistributionEstate';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
@@ -66,10 +62,6 @@ const App = () => {
             </Route>
             <Route path='/about-us' component={Team}/>
             <Route path='/methodology' component={Methodology}/>
-            <Route path='/availablebeds' component={AvailableBeds}/>
-            <Route path='/confirmage' component={ConfirmAge}/>
-            <Route path='/municipalitiesfollow' component={MunicipalitiesFollow}/>
-            <Route path='/distributionestate' component={DistributionEstate}/>
             <Route path='/login'>
               {loggedIn ? 
                 <React.Fragment>
